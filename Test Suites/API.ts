dfe99fee89da236a8c9c42e5c4b44653b30a0d18
Ahmad Aqil <johndoe@example.com>
2023-02-08 11:10:20 +0700

    katalon succes

---
+++ Test Suites/API.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>API</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>e75e8c94-61ad-45d0-a9c7-67ead1a4bddb</testSuiteGuid>
+</TestSuiteEntity>
